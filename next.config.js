b236a70b578e257e99113ea86dc0b3325f0c1790
Thipeix <47480315+progtitos@users.noreply.github.com>
2026-08-30 12:21:19 -0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    serverActions: { bodySizeLimit: "25mb" }, // uploads de PDFs de processos grandes
-  },
-};
-
-module.exports = nextConfig;
